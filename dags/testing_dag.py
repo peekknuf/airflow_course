ba824633427285b0c3506d6dde773db36c02c30c
max <peekknuf@gmail.com>
2024-11-02 11:22:14 +0100

    astro airflow tutorial course

---
+++ dags/testing_dag.py
@@ -0,0 +1,41 @@
+from airflow.decorators import dag, task 
+from datetime import datetime, timedelta
+from time import sleep
+
+default_args = {
+    "owner": "maksym ionutsa",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 1, 1),
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=5)}
+
+@dag (
+    default_args=default_args,
+    dag_id="testing_dag",
+    description="Testing DAG",
+    schedule_interval=timedelta(days=1),
+    dagrun_timeout=timedelta(minutes=60),
+    tags=["example", "testing"],
+    catchup=False)
+
+def my_dag():
+
+    @task
+    def print_hello():
+        print("hello world")
+
+    @task
+    def wait():
+        sleep(3)
+
+    @task
+    def print_goodbye():
+        print("goodbye world")
+
+   
+    print_hello() >> wait() >> print_goodbye()
+
+my_dag()
+
